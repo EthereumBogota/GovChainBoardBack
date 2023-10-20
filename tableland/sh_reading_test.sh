bcc5cac674559b830ba3dcc26e9e6fa5beaef0f8
Daniel Santiago Sandoval Higuera <95585487+hisandan@users.noreply.github.com>
2023-10-20 22:35:28 +0000

    :sparkles: Exploring Tableland and Basin-Cli

--- tableland/sh_reading_test.sh
+++
@@ -1,4 +0,0 @@
-curl -X GET https://testnets.tableland.network/api/v1/tables/80001/1 \
-  -H 'Accept: application/json'
-curl -X GET https://testnets.tableland.network/api/v1/tables/80001/govchainboard2.users \
-  -H 'Accept: application/json'
